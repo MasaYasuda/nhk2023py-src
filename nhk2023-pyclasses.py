fd0d094f21db0f3db635001124dfb75fe7e3a5e5
masayasuda <masayasuda137@gmail.com>
2023-02-12 00:17:31 +0900

    ok

--- nhk2023-pyclasses.py
+++ nhk2023-pyclasses.py
@@ -92,6 +92,7 @@ class Motor:
       self.__roller_enc_gear_ratio = gear_ratio
       
     def calc_roller_speed(self,spin): # spin: -1~1
+      self.roller_speed[i]=self.__max_spin_speed * spin *30000 / self.__roller_diameter /math.pi # N[rpm]=30*v[m/s]/(r[mm]*pi)
       return
     def calc_roller_enc_target(self):
       self.roller_enc_target = self.__roller_enc_gear_ratio * self.roller_speed
