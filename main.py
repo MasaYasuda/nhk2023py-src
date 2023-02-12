637b1aa0c8c515278ac899b6fba023a54692ec98
minia <robocon.gifu2@gmail.com>
2023-02-13 00:02:08 +0900

    ok

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ print("コントローラのボタンを押してください")
 try:
     vector=nhk23.Vector() # make instance
     motor=nhk23.Motor("omni") # make instance
-    motor.omni_setup(100,500,1,1,1000,[1,1,1,1])
-    motor2=nhk23.Motor("roller") # make instance
-    motor2.roller_setup(50,1,1)
+    motor.omni_setup(5,50,0.0001,0.1,1000,[1,1,1,1])
+    #motor2=nhk23.Motor("roller") # make instance
+    #motor2.roller_setup(50,1,1)
     transmitter = nhk23.Transmitter("/dev/ttyACM0", 115200)
     
     while True:
         ## Get Inputs
         events = pygame.event.get()
-        print("右スティック座標")
-        print("("+str(j.get_axis(2))+","+ str(j.get_axis(3))+")")
+        print("左スティック座標")
+        print("("+str(j.get_axis(0))+","+ str((-1)*j.get_axis(1))+")")
         print("右スティックx座標")
         print(str(j.get_axis(3)))
+        
+        '''
         print("R2の押し込み量")
         print(j.get_axis(5))
+        '''
         
         x=j.get_axis(0)
         y=j.get_axis(1)*(-1)
@@ -35,11 +38,13 @@ try:
         move,rot = vector.calc_vector(x,y,rotation)  # calc.vector using  x,y,rotation
         omni_output = motor.calc_omni_output(move,rot)  # move,rot is "Vector.move","Vector.rot"
         print(omni_output)
+        '''
         spin_output = motor2.calc_roller_output(spin) # spin: -1~1 
         print(spin_output)
-        
+        '''
         transmitter.write_all_auto([0,1,2,3],motor.omni_enc_target)
-        transmitter.write_single_auto(5,motor2.roller_enc_target)
+
+        # transmitter.write_single_auto(5,motor2.roller_enc_target)
         
         time.sleep(0.01)
         
--- motorslave/src/crossblink.cpp
+++ motorslave/src/crossblink.cpp
@@ -2,10 +2,11 @@
 
 void setup(){
   Serial.begin(11520);
-  float order_rpm[6]={0};
-  const int pinPWM[4]={2,3,4,5};
-  int output_value[4]={0,0,0,0};
 }
+float order_rpm[4]={0};
+const int pinPWM[4]={2,3,4,5};
+int output_value[4]={0,0,0,0};
+
 void loop(){
   if (Serial.read() == 0xFF)
     {
@@ -14,17 +15,17 @@ void loop(){
       if(order_rpm[data1]>1){order_rpm[data1]=1;}
       else if(order_rpm[data1]<1){order_rpm[data1]=-1;}
       output_value[data1]=int(order_rpm[data1]*255);
-      analogwrite(pinPWM[data1],order_rpm[data1]);
+      analogWrite(pinPWM[data1],order_rpm[data1]);
 
     }
 
   printf("[");
   for (int i=0;i<4;i++){
-    printf(order_rpm[i]);
+    printf("%f",order_rpm[i]);
     if(i==3){
       break;
     }
-    printf(,);
+    printf(",");
   }
   printf("]");
 }
