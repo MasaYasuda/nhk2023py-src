3b8762725a93283743e5aded16aab92f15c2fd7b
masayasuda <masayasuda137@gmail.com>
2023-02-14 11:16:53 +0900

    ok

--- ms/ms/src/crossblink.cpp
+++ ms/ms/src/crossblink.cpp
@@ -4,6 +4,12 @@
 const int motorCount = 6;
 float motorSpeeds[motorCount];
 int pinPWM[4] = {2, 3, 4, 5};
+float bytesToFloat(byte *buffer) {
+  float value;
+  memcpy(&value, buffer, sizeof(value));
+  return value;
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -27,33 +33,32 @@ void loop()
         byte value1=Serial.read();
         byte value2=Serial.read();
         byte value3=Serial.read();
+        byte buffer[4]={value0,value1,value2,value3};
+        float value=bytesToFloat(buffer);
         //speed = Serial.read() | (Serial.read() << 8) | (Serial.read() << 16) | (Serial.read() << 24);
-        speed=float(value0|(value1<<8)|(value2<<16)|(value3<<24));
+        //speed=(float)(value3|((0x0000|value2)<<8) | ((0x000000 | value1 ) << 16)|((0x00000000|value0)<<24));
         Serial.println(value0);
-        Serial.println(value0);
-        Serial.println(value0);
-        Serial.println(value0);
-        Serial.println(speed);
+        Serial.println(value1);
+        Serial.println(value2);
+        Serial.println(value3);
+        //Serial.println(speed);
+        Serial.println(value);
+
         //motorSpeeds[motorNumber] = speed;
+        motorSpeeds[motorNumber] = value;
         Serial.print("Motor: ");
         Serial.print(motorNumber);
         Serial.print(", Speed: ");
         Serial.println(motorSpeeds[motorNumber]);
-        /*
-        if (abs(motorSpeeds[motorNumber]) > 1)
-        {
-          motorSpeeds[motorNumber] = 0;
-        }
-        int output_value = 240 * (motorSpeeds[motorNumber]);
-        analogWrite(pinPWM[motorNumber], output_value);
-        */
-        int count=(int)(value);
+
+        int count=(int)(speed);
         for(int i=0;i<count;i++){
             digitalWrite(13,HIGH);
             delay(200);
             digitalWrite(13,HIGH);
             delay(200);
         }
+        while (Serial.available())Serial.read();
       }
     }
   }
