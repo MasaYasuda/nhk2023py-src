40b1477a24a5f4c581685e9e550bfbb619a46300
masayasuda <masayasuda137@gmail.com>
2023-02-14 16:58:31 +0900

    ok

--- ms/ms/src/classes.h
+++ ms/ms/src/classes.h
@@ -2,8 +2,10 @@
 #define _CLASSES_H_
 
 #include <Arduino.h>
+
 // 構造体宣言
-typedef union {
+typedef union
+{
   float val;
   byte binary[4];
 } uf;
@@ -12,76 +14,79 @@ typedef union {
 class CSample
 {
 public:
-    void set(int num);  // m_numに値を設定する
-    int get();          // m_numの値を取得する
+  void set(int num); // m_numに値を設定する
+  int get();         // m_numの値を取得する
 private:
-    int m_num;
+  int m_num;
 };
 
-class Power{
-  public:
-    //  コンストラクタ
-    Power();
-    void output();
-    int *getOutput_dir();
-    int *getOutput_pwm();
+class Power
+{
+public:
+  //  コンストラクタ
+  Power();
+  void output();
+  int *getOutput_dir();
+  int *getOutput_pwm();
 
-  private:
-    int pin_dir[6];
-    int pin_pwm[6];
-    int output_dir[6];
-    int output_dir[6];
+private:
+  int pin_dir[6];
+  int pin_pwm[6];
+  int output_dir[6];
+  int output_dir[6];
 };
 
-class TimerPID{
-  public:
-    TimerPID();
-    void calc_speed(int count[6]);
-    void calc_pid(float order_speed[6]);
-    float *getPower_rate();
+class TimerPID
+{
+public:
+  TimerPID();
+  void calc_speed(int count[6]);
+  void calc_pid(float order_speed[6]);
+  float *getPower_rate();
 
-  private:
-    int count_past[6];
-    int resolution;
-    float speed_past[6];
-    float Kp;
-    float Ki;
-    float Kd;
-    int dev_past[6];
-    int integtal[6];
-    float power_rate[6];
+private:
+  int count_past[6];
+  int resolution;
+  float speed_past[6];
+  float Kp;
+  float Ki;
+  float Kd;
+  int dev_past[6];
+  int integtal[6];
+  float power_rate[6];
 };
 
-class Encoder{
-  public:
-    Encoder();
-    int *getCount();
-    void pinInterrupt0R();
-    void pinInterrupt1R();
-    void pinInterrupt2R();
-    void pinInterrupt3R();
-    void pinInterrupt4R();
-    void pinInterrupt5R();
+class Encoder
+{
+public:
+  Encoder();
+  int *getCount();
+  void pinInterrupt0R();
+  void pinInterrupt1R();
+  void pinInterrupt2R();
+  void pinInterrupt3R();
+  void pinInterrupt4R();
+  void pinInterrupt5R();
 
-    void pinInterrupt0F();
-    void pinInterrupt1F();
-    void pinInterrupt2F();
-    void pinInterrupt3F();
-    void pinInterrupt4F();
-    void pinInterrupt5F();
+  void pinInterrupt0F();
+  void pinInterrupt1F();
+  void pinInterrupt2F();
+  void pinInterrupt3F();
+  void pinInterrupt4F();
+  void pinInterrupt5F();
 
-  private:
-    int count[6];
+private:
+  int count[6];
 };
 
-class Receiver:public Serial{
-  public:
-    Receiver(int baudrate);
-    float *getOrder_speed();
-    void read_order();
-  
-  private
-    uf order_speed[6];
+class Receiver : public Serial
+{
+public:
+  Receiver(int baudrate);
+  float *getOrder_speed();
+  void read_order();
+
+private
+  uf order_speed[6];
 }
 #endif //_CLASSES_H_
-
