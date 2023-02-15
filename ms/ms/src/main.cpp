6e94ddf891f01637f23282e67afff191ae450473
masayasuda <masayasuda137@gmail.com>
2023-02-15 11:42:56 +0900

    ok

--- ms/ms/src/main.cpp
+++ ms/ms/src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
 #include <TimerOne.h>
 #include "classes.h"
-
+float i=0.0;
 //　変数・オブジェクト宣言
-Receiver receiver(115200);
 Power power;
 void setup(){
-  encoder_setup();
-  timer_setup();
+  Serial.begin(115200);
 }
 void loop(){
-  receiver.read_order();
+
+  power_rate[2]=i;
   power.output(power_rate);
+  int *pwm=power.getOutput_pwm();
+  Serial.println(pwm[2]);
+  delay(100);
+  i+=i+0.01;
+  if(i>1){i=0;}
 }
