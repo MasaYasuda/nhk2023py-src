ef916f21ec8dc0833b9ce18233a2e19a6a41d353
matushitaB <matushitaB@robocon.gifu2@gmail.com>
2023-02-17 15:39:43 +0900

    ok

--- nhk23.py
+++ nhk23.py
@@ -168,12 +168,19 @@ class Motor:
   
 class Transmitter (serial.Serial):
   """
+  this class is specified for ArduinoMegaMotorSlave.
+
   HOW TO USE Transmiter class
   
+
+
   ## AUTOMATIC
 
   transmitter = nhk23.Transmitter("COM3", 115200)
   motor_num_array=[0,1,2,3,4,5]
+  # If speed pid
+  write_mode_auto=[20,20,20,20,20,20]
+  write_direction_config =[0,0,0,0,0,0]
   values=[10,10,10,10,10,10]
   transmitter.write_all_auto(motor_num_array,values)
   transmitter.close()  ## DON'T FORGET!!
@@ -193,9 +200,12 @@ class Transmitter (serial.Serial):
   transmitter.close()   ## DON'T FORGET!!
   
   """
+  
   def __init__(self, port, baudrate):
         super().__init__(port, baudrate) # 基底クラスのコンストラクタをオーバーライド
         self.target_values = [0,0,0,0,0,0]
+        self.target_mode =[100,100,100,100,100,100]
+        self.target_direction_config=[0,0,0,0,0,0]
         
   def store_single_target_values(self,motor_num,value):  # motor_num starts 0 (max 5)
     self.target_values[motor_num]=float(value)
@@ -203,6 +213,7 @@ class Transmitter (serial.Serial):
   def store_all_taget_values(self,motor_num_array,values):
     for i in range(0,len(motor_num_array)):
       self.target_values[motor_num_array[i]]=float(values[i])
+  
       
   def write_single(self,motor_num):
     # 1Byte目は固定値0xFF
@@ -228,6 +239,51 @@ class Transmitter (serial.Serial):
     self.store_single_target_values(motor_num,value)
     self.write_single(motor_num)
     return 
+  
+  def write_mode_auto(self,mode_array):
+    '''
+    motor_num=0 to 5
+    define mode num  =100 to 105
+    
+    mode:
+    0... radio controll ->
+    10... position pid
+    20... speed pid
+    100(default) ... output disabled
+
+    '''
+    for i in range(0,6):
+      self.target_mode[i]=float(mode_array[i])
+      # 1Byte目は固定値0xFF
+      # 2Byte目は1Byteデータ（数値）
+      # 3~6Byte目はfloat型数値
+      data = [0xFF, (i+100).to_bytes(1,"big"),self.target_mode[i]]
+      packet = struct.pack('>Bcf', *data)
+      self.write(packet)
+      print(data)
+      print(str(packet))
+      return data
+  
+  def write_direction_config(self,config_array):
+    '''
+    motor_num = 0 to 5
+    config_num = 200 to 205
+    
+    To know the config number, please refer to this ""
+
+    '''
+    for i in range(0,6):
+      self.target_direction_config[i]=float(config_array[i])
+      # 1Byte目は固定値0xFF
+      # 2Byte目は1Byteデータ（数値）
+      # 3~6Byte目はfloat型数値
+      data = [0xFF, (i+200).to_bytes(1,"big"),self.target_direction_config[i]]
+      packet = struct.pack('>Bcf', *data)
+      self.write(packet)
+      print(data)
+      print(str(packet))
+      return data
+
 
 """  
 class Dynamixel: ## This class is specified in X_series
