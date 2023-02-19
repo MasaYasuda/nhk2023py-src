4286d361c89d66dcce66bbe024e584a18656d13d
minia <robocon.gifu2@gmail.com>
2023-02-19 21:21:10 +0900

    ok

---
+++ NHK23_move_and_rot(radicon).py
@@ -0,0 +1,66 @@
+import nhk23
+import pygame
+import time
+import os
+try:
+    os.environ['SDL_VIDEODRIVER'] = 'dummy'
+    
+    pygame.init()
+    j = pygame.joystick.Joystick(0)
+    j.init()
+
+    ##### VECTOR SETUP
+    
+    vector=nhk23.Vector() # make instance
+    
+    ##### MOTOR SETUP
+
+    motor=nhk23.Motor("omni") # make instance
+    
+    ##### TRANSMITTER SETUP
+
+    transmitter = nhk23.Transmitter("/dev/ttyACM0", 115200)
+
+    # If speed pid
+    mode_array=[0,0,0,0,100,100]
+    direction_config_array =[0,3,3,0,0,0] #回転が逆だったら3にする
+    forward_direction_array=[0,0,0,0,0,0]
+    transmitter.write_config_all(mode_array,direction_config_array,forward_direction_array)
+
+    print("コントローラのボタンを押してください")
+    while True:
+        
+        transmitter.reset_input_buffer()
+        ## Get Inputs
+        events = pygame.event.get()
+        print("左スティック座標")
+        print("("+str(j.get_axis(0))+","+ str((-1)*j.get_axis(1))+")")
+        print("右スティックx座標")
+        print(str(j.get_axis(3)))
+        
+        ##### VECTOR CALCLATION
+        x=j.get_axis(0)
+        y=j.get_axis(1)*(-1)
+        rotation=j.get_axis(3)
+        move,rot = vector.calc_vector(x,y,rotation)  # calc.vector using  x,y,rotation
+        
+        ##### MOTOR CALCLATION
+        omni_output = motor.calc_omni_output_for_radicon(move,rot)  # move,rot is "Vector.move","Vector.rot"
+        print(omni_output)
+        
+        ##### TRANSMIT 
+        transmitter.write_all_auto([0,1,2,3],omni_output)
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("プログラムを終了します")
+    init_array=[0,0,0,0,0,0]
+    transmitter.write_all_auto([0,1,2,3,4,5],init_array)
+    j.quit()
+    transmitter.close()
+    
+
+
+
+
