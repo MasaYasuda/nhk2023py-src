1a898f3db9917ba16c03c58ee83b7a10396a1045
masayasuda <masayasuda137@gmail.com>
2023-02-23 12:12:11 +0900

    ok

--- ms/shelter_low_level/test_pwm.cpp
+++ ms/shelter_low_level/test_pwm.cpp
@@ -2,21 +2,21 @@
 
 void setup(){
     Serial.begin(115200);
-    pinMode(31,OUTPUT);
-    digitalWrite(31,LOW);
+    pinMode(30,OUTPUT);
+    digitalWrite(30,LOW);
 }
 void loop(){
-    digitalWrite(31,HIGH);
-    analogWrite(7,0);
+    digitalWrite(30,HIGH);
+    analogWrite(6,0);
     Serial.println("POWER:0");
     delay(2000);
-    analogWrite(7,100);
+    analogWrite(6,100);
     Serial.println("POWER:25");
     delay(2000);
-    analogWrite(7,200);
+    analogWrite(6,200);
     Serial.println("POWER:50");
     delay(2000);
-    analogWrite(7,100);
+    analogWrite(6,100);
     Serial.println("POWER:25");
     delay(2000);
 }
