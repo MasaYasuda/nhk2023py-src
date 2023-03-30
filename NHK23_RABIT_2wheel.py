6c5c1d4b0d8901fe0cf65e5df0e857757b0b52d9
masayasuda <masayasuda137@gmail.com>
2023-03-30 12:16:33 +0900

    ok

---
+++ NHK23_RABIT_2wheel.py
@@ -0,0 +1,70 @@
+import nhk23
+import pygame
+import time
+import os
+try:
+    os.environ['SDL_VIDEODRIVER'] = 'dummy'
+    
+    pygame.init()
+    j = pygame.joystick.Joystick(0)
+    j.init()
+
+    ##### TRANSMITTER SETUP
+
+    transmitter = nhk23.Transmitter("/dev/ArduinoMega1", 115200)
+
+    # If speed pid
+    mode_array=[0,0,100,100,100,100]
+    direction_config_array =[3,3,0,0,0,0] #回転が逆だったら3にする
+    forward_direction_array=[1,1,1,1,1,1]
+
+    transmitter.write_config_all(mode_array,direction_config_array,forward_direction_array)
+
+    print("コントローラのボタンを押してください")
+    while True:
+        transmitter.reset_input_buffer()
+        
+        ## Get Inputs
+        events = pygame.event.get()
+        
+        ##### VECTOR CALCLATION
+        move=j.get_axis(1)*(-1)
+        rotation=j.get_axis(3)*0.3
+        
+        # JoyStickのわずかな傾きは無視する
+        if abs(move) < 0.1:
+            move = 0
+        if abs(rot) < 0.05:
+            rot = 0
+
+        # 運動学（簡易版）の計算
+        R_value = move - rot
+        L_value = move + rot
+
+        # 大きすぎる値（1以上）のとき圧縮する
+        R_abs = abs(R_value)  # 絶対値を求める
+        L_abs = abs(L_value)  # 絶対値を求める
+
+        if R_abs > 1 or L_abs > 1:
+            if R_abs >= L_abs:
+                R_value = R_value / R_abs  # 圧縮
+                L_value = L_value / R_abs  # 圧縮
+            elif R_abs < L_abs:
+                R_value = R_value / L_abs
+                L_value = L_value / L_abs
+
+        ##### TRANSMIT 
+        transmitter.write_single_auto(0,R_value)
+        time.sleep(0.05)
+        transmitter.write_single_auto(1,L_value)
+        time.sleep(0.05)
+
+
+
+except KeyboardInterrupt:
+    print("プログラムを終了します")
+    j.quit()
+    init_array=[0,0,0,0,0,0]
+    transmitter.write_all_auto([0,1,2,3,4,5],init_array)
+    transmitter.close()
+    
